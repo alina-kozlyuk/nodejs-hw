af52925d8b7d536bcd8265bbc783569c880eccf7
Alina Kozlyuk <talina787@icloud.com>
2026-07-21 12:58:47 +0300

    authentication and authorization

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -5,7 +5,7 @@ export const getAllNotes = async (req, res) => {
   const { page = 1, perPage = 10, tag, search } = req.query;
   const skip = (page - 1) * perPage;
 
-  const myQuery = Note.find();
+  const myQuery = Note.find({userId: req.user._id});
 
   if (search) {
     myQuery.where({
@@ -38,7 +38,10 @@ export const getAllNotes = async (req, res) => {
 
 export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findById(noteId);
+  const note = await Note.findOne({
+    _id: noteId,
+    userId: req.user._id,
+  });
 
   if (!note) {
     throw createHttpError(404, 'Note not found');
@@ -48,14 +51,19 @@ export const getNoteById = async (req, res) => {
 };
 
 export const createNote = async (req, res) => {
-  const note = await Note.create(req.body);
+  const note = await Note.create({
+    ...req.body,
+    userId: req.user._id,
+  });
+
   res.status(201).json(note);
 };
 
 export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findOneAndDelete({
-    _id: noteId,
+   _id: noteId,
+    userId: req.user._id,
   });
 
   if (!note) {
@@ -69,7 +77,8 @@ export const updateNote = async (req, res) => {
   const { noteId } = req.params;
 
   const note = await Note.findOneAndUpdate(
-    { _id: noteId },
+    { _id: noteId,
+    userId: req.user._id, },
     req.body,
     { returnDocument: "after" },
   );
---
+++ src/models/session.js
@@ -0,0 +1,30 @@
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+
+export const sessionSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    accessToken: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+    accessTokenValidUntil: {
+      type: Date,
+      required: true,
+    },
+    refreshTokenValidUntil: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Session = model('Session', sessionSchema);
