fa810001f999d9965b348525b40e32b62963aa00
Alina Kozlyuk <talina787@icloud.com>
2026-07-21 13:27:58 +0300

    edits

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -2,7 +2,6 @@ import { Joi, Segments } from "celebrate";
 
 export const registerUserSchema = {
   [Segments.BODY]: Joi.object({
-    username: Joi.string().optional(),
     email: Joi.string().email().required(),
     password: Joi.string().min(8).required(),
   }),
