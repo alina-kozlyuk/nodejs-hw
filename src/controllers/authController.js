07b28816b670c90a0a88e6cf1ddb480b3ecf5b92
Alina Kozlyuk <talina787@icloud.com>
2026-07-26 17:34:53 +0300

    edits

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,7 +8,7 @@ import fs from 'node:fs/promises';
 import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from "../services/auth.js";
 import { Session } from "../models/session.js";
-import { sendMail } from "../utils/sendMail.js";
+import { sendEmail } from "../utils/sendMail.js";
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -129,7 +129,7 @@ export const requestResetEmail = async (req, res, next) => {
   });
 
   try {
-    await sendMail({
+    await sendEmail({
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
