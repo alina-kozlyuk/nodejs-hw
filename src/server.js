57e4b130a08e17b17f79bb38df80767a5538b804
Alina Kozlyuk <talina787@icloud.com>
2026-07-26 16:43:04 +0300

    add reset password and avatar

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -20,6 +22,7 @@ app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
